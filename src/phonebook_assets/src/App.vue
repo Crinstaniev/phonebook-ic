0ffe9ee851e0899fec6c9f551a171f3e7e0877a3
Crinstaniev <zhuangzesen@gmail.com>
2021-07-27 11:23:18 +0800

    [UPD] integrate login func

--- src/phonebook_assets/src/App.vue
+++ src/phonebook_assets/src/App.vue
@@ -1,9 +1,46 @@
 <template>
-  <div>Hello, World!</div>
+  <!-- Test of authorized call -->
+  <div>
+    <button @click="handleLogin">Login</button>
+    <div>{{ principal }}</div>
+  </div>
 </template>
 
 <script>
-export default {};
+import { AuthClient } from "@dfinity/auth-client";
+import { canisterId, createActor } from "../../declarations/phonebook";
+
+export default {
+  data() {
+    return {
+      principal: "",
+    };
+  },
+
+  methods: {
+    async handleLogin() {
+      const authClient = await AuthClient.create();
+      authClient.login({
+        onSuccess: async () => {
+          const identity = authClient.getIdentity();
+          const actor = createActor(canisterId, {
+            agentOptions: {
+              identity,
+            },
+          });
+
+          const res = await actor.callerPrincipal();
+          console.log(res);
+        },
+
+        identityProvider:
+          process.env.DFX_NETWORK === "ic"
+            ? "https://identity.ic0.app/#authorize"
+            : process.env.LOCAL_II_CANISTER,
+      });
+    },
+  },
+};
 </script>
 
 <style>
